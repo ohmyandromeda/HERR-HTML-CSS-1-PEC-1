e43c773bf0c57249fe2b040663a16ddbb1b18867
Andrea <anesher99@gmail.com>
2025-04-03 19:44:15 +0200

    Nuevo inicio

---
+++ web/javascript/script.js
@@ -0,0 +1,17 @@
+const buscador = document.getElementById('buscar-palabra')
+const recetas = document.querySelectorAll('.receta')
+
+buscador.addEventListener('input', (e) => {
+    const palabraIntroducida = e.target.value.toLowerCase().trim();
+    console.log("Buscando:", palabraIntroducida); 
+
+    recetas.forEach(receta => {
+        const nombreReceta = receta.dataset.receta;
+        console.log("Receta encontrada:", nombreReceta); 
+        if (nombreReceta && nombreReceta.toLowerCase().includes(palabraIntroducida)) {
+            receta.style.display = 'flex'; 
+        } else {
+            receta.style.display = 'none'; 
+        }
+    })
+})
